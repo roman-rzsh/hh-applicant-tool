58784c227b9bb011918724945a7acf945ddafb41
roman_zh1 <roman_zh1@vk.com>
2026-03-22 17:01:53 +0400

    fix: remove force letters, period 7

--- scripts/apply_all.sh
+++ scripts/apply_all.sh
@@ -14,11 +14,10 @@ docker compose run -u docker -T hh_applicant_tool hh-applicant-tool \
 --experience between3And6 \
 --salary 250000 \
 --area 113 \
---period 14 \
+--period 7 \
 --per-page 33 \
 --total-pages 5 \
 --order-by publication_time \
---force-message \
 --letter letter.txt \
 --resume-id 68cca835ff1023f0050039ed1f6d42676d5151
 # --schedule remote
@@ -32,11 +31,10 @@ docker compose run -u docker -T hh_applicant_tool hh-applicant-tool \
 --experience moreThan6 \
 --salary 250000 \
 --area 113 \
---period 20 \
+--period 7 \
 --per-page 33 \
 --total-pages 5 \
 --order-by publication_time \
---force-message \
 --letter letter.txt \
 --resume-id 68cca835ff1023f0050039ed1f6d42676d5151
 # --schedule remote
@@ -49,11 +47,10 @@ docker compose run -u docker -T hh_applicant_tool hh-applicant-tool \
 --professional-role 96 156 160 \
 --area 5 40 9 16 28 48 97 1001 \
 --experience between3And6 \
---period 20 \
+--period 7 \
 --per-page 33 \
 --total-pages 5 \
 --order-by publication_time \
---force-message \
 --letter letter-eng.txt \
 --resume-id 19fc98a7ff103625e50039ed1f526171704c65
 # --schedule remote
@@ -64,13 +61,12 @@ docker compose run -u docker -T hh_applicant_tool hh-applicant-tool \
 --search 'go разработчик OR golang разработчик OR go developer OR golang developer' \
 --search-field name \
 --professional-role 96 156 160 \
---experience between3And6 \
+--experience between1And3 \
 --salary 250000 \
---period 20 \
+--period 7 \
 --per-page 33 \
 --total-pages 5 \
 --order-by publication_time \
---force-message \
 --letter letter-mid.txt \
 --resume-id 87838141ff103495b80039ed1f4c5137315263
 # --schedule remote
